fa26f8787e65899c7ebdbc6ab54c2094aa81d639
Sebastien Langoureaux <sebastien.langoureaux@harmonie-mutuelle.fr>
2020-02-13 10:41:01 +0000

    Fix locals

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 locals {
-  role_read_id    = "${compact(concat(coalescelist(kibana_role.read.*.id), list("")))}"
-  role_read_name  = "${compact(concat(coalescelist(kibana_role.read.*.name), list("")))}"
+  output_role_read_id    = "${compact(concat(coalescelist(kibana_role.read.*.id), list("")))}"
+  output_role_read_name  = "${compact(concat(coalescelist(kibana_role.read.*.name), list("")))}"
 }
 
 
@@ -21,9 +21,9 @@ output "role_write_name" {
 }
 
 output "role_read_id" {
-  value = "${join("", local.role_read_id)}"
+  value = "${join("", local.output_role_read_id)}"
 }
 
 output "role_read_name" {
-  value = "${join("", local.role_read_name)}"
+  value = "${join("", local.output_role_read_name)}"
 }
