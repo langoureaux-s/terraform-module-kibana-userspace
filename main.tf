455c3d3cfc41f311ec21dab36cf8bbf6f8061e65
Sebastien Langoureaux <sebastien.langoureaux@harmonie-mutuelle.fr>
2020-02-13 10:49:10 +0000

    Fix locals

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@ locals {
     name            = "${replace(var.name, "/[ \\-]/", "_")}"
     role_write_name = "space_${local.name}_write"
     role_read_name  = "space_${local.name}_read"
-    groups_write    = "{\"any\":[${replace(formatlist("{\"field\":{\"groups\":\"%s\"}},", var.groups_write), "/,$/", "")}]}"
-    groups_read     = "{\"any\":[${replace(formatlist("{\"field\":{\"groups\":\"%s\"}},", var.groups_read), "/,$/", "")}]}"
+    groups_write    = "{\"any\":[${join(formatlist("{\"field\":{\"groups\":\"%s\"}}", var.groups_write), ",")}]}"
+    groups_read     = "{\"any\":[${join(formatlist("{\"field\":{\"groups\":\"%s\"}}", var.groups_read), ",")}]}"
 }
